644e96a3f9577a0f42ef1fd52d9035ad8556a8bf
Shahzaib KK <shahzaib84.sk@gmail.com>
2023-04-29 15:54:17 +0500

    working

--- chapter6_patterns/PROJECT_phone_email.py
+++ chapter6_patterns/PROJECT_phone_email.py
@@ -3,24 +3,26 @@ import re, pyperclip
 
 phone_regex = re.compile(
     r"""(
-    (\+\d\d\s\d{3})?
-    (\d{4}\(\d{4}\))?  # Area Code
-    (\s|-|\.)?          # Separator
-    (\d{7})             # Main Number
-)""",
+    (\+\d{2}\s\d{3})?        # Country code
+    (\d{4}|\(\d{4}\))?       # Area code
+    (\s|-|\.)?               # Separator
+    (\d{11}|\d{7})           # Main number
+    )""",
     re.VERBOSE,
 )
 
-# mo = phone_regex.search("+92 316 9743421")
+# mo = phone_regex.search("+92 316 9743421")
 # print(mo.group())
+# mo = phone_regex.findall("+92 316 9743421")
+# print(mo)
 
 email_regex = re.compile(
-    r"""
+    r"""(
     [a-zA-Z0-9._%+.]+    # username
     @                    # @ symble
     [a-zA-Z0-9.-]+      # domain name
     (\.[a-zA-Z]{2,4})   # dot-something
-""",
+)""",
     re.VERBOSE,
 )
 
@@ -30,4 +32,12 @@ text = str(pyperclip.paste())
 matches = []
 
 for groups in phone_regex.findall(text):
-    pass
+    matches.append(groups[0].strip())
+for egroups in email_regex.findall(text):
+    matches.append(egroups[0].strip())
+
+print(matches)
+
+if len(matches) > 0:
+    pyperclip.copy("\n".join(matches))
+    print("Copied")

a58dc8d2c482b2d3e101df60d0d07410bdc21bbf
Shahzaib KK <shahzaib84.sk@gmail.com>
2023-08-28 18:09:29 +0500

    Debugging

---
+++ chapter10_debugging/assertions__.py
@@ -0,0 +1,4 @@
+age = [2, 48, 54, 12, 4, 9, 7, 22]
+age.sort()
+print(age)
+assert age[0] >= age[-1], "Error"
--- chapter10_debugging/raise.py
+++ chapter10_debugging/raise.py
@@ -1,1 +1,8 @@
-raise Exception("Fuck Bro")
+import traceback
+
+try:
+    raise Exception("Fuck Bro")
+except:
+    with open("traceback.txt", "w") as error_file:
+        error_file.write(traceback.format_exc())
+        print(f"The Error Message in traceback.txt")

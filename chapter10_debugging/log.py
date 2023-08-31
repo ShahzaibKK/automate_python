5a0f6f306b2acfaca897024d7b763dbbc92f127c
Shahzaib KK <shahzaib84.sk@gmail.com>
2023-08-31 18:30:32 +0500

    Hartal

---
+++ chapter10_debugging/log.py
@@ -0,0 +1,14 @@
+import logging
+
+logging.basicConfig(
+    filename="my_log.txt",
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+# Now log messages will be saved to 'myProgramLog.txt'
+logging.debug("Debug message")
+logging.info("Info message")
+logging.warning("Warning message")
+logging.error("Error message")
+logging.critical("Critical message")

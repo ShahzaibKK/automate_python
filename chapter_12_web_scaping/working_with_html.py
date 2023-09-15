bb6f0fdfb7c4a6d43393d1b2ebadfa04eff5a93c
Ma_win11 <shahzaib84.sk@gmail.com>
2023-09-15 23:08:15 +0500

    homework

---
+++ chapter_12_web_scaping/working_with_html.py
@@ -0,0 +1,23 @@
+import requests, bs4
+
+# from Internet
+res = requests.get("https://nostarch.com")
+res.raise_for_status()
+no_starch_soup = bs4.BeautifulSoup(res.text, "html.parser")
+print(type(no_starch_soup))
+# from file in hard drive
+example_file = open("example.html")
+example_soup = bs4.BeautifulSoup(example_file.read(), "html.parser")
+elmts = example_soup.select("p")
+print(type(elmts))
+print(len(elmts))
+print(type(elmts[0]))
+print(str(elmts[0]))
+print(elmts[0].get_text())
+print(elmts[0].attrs)
+print(str(elmts[1]))
+print(elmts[1].get_text())
+print(elmts[1].attrs)
+print(str(elmts[2]))
+print(elmts[2].get_text())
+print(elmts[2].attrs)
